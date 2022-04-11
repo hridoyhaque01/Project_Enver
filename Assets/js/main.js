3bf56ea5577f8c550d9067efcf9c45528039d316
Hridoy <iamhridoyhaque.com>
2022-04-11 16:40:17 +0600

    complete web design for desktop version

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,13 +1,47 @@
-const glightbox = GLightbox({
+
+
+  /*========================== SHOW NAVBAR WHEN IT'S SCROLL ==============================*/
+    const navbar = document.querySelector('.navbar')
+    function showNav(){
+      let scrollY = window.scrollY
+      if(scrollY > 60){
+        navbar.classList.add('show_navbar')
+      }else{
+        navbar.classList.remove('show_navbar')
+      }
+      console.log(scrollY)
+    }
+
+  window.addEventListener('load', showNav)
+  window.addEventListener('scroll', showNav)
+
+  /*===================> IMPLEMENT LIGHTBOX JS FOR VIDEO POPUP ========================*/
+
+  const glightbox = GLightbox({
     selector: '.glightbox'
   });
 
+  /*===================> IMPLEMENT OWL-CAROUSEL JS FOR SLIDER BOX ========================*/
 
-  var swiper = new Swiper(".portfolio-content", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:50,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+              nav:true
+          },
+          767:{
+              items:3,
+              nav:false
+          },
+          991:{
+              items:4,
+              nav:true,
+              loop:false
+          }
+      }
+  })
+
+  
